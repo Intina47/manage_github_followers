695c9cc4d43df136ecb8954f1646dd0345ff8ec3
mamba <isaiahntina47@gmail.com>
2024-05-27 22:08:34 +0000

    chore: Remove unnecessary script tag from layout.js

--- github-followers-manager/app/layout.js
+++ github-followers-manager/app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import Head from 'next/head';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,9 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <script src="https://unpkg.com/htmx.org@1.6.1"></script>
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
