8e7e5d8eb7cc9ce648c01e150421ebdf00a555a8
mamba <isaiahntina47@gmail.com>
2024-05-27 22:08:54 +0000

    chore: Update API endpoints to include pagination for followers and following

--- github-followers-manager/pages/api/followers.js
+++ github-followers-manager/pages/api/followers.js
@@ -1,50 +1,55 @@
 const axios = require('axios');
-
+  
 export default async (req, res) => {
   if (req.method === 'POST') {
-    const { username } = req.body;
+    const { username , page = 1} = req.body;
     console.log("Received username: ", username);
 
     try {
-      const followersRes = await axios.get(`https://api.github.com/users/${username}/followers`);
-      const followingRes = await axios.get(`https://api.github.com/users/${username}/following`);
+      const followersRes = await axios.get(`https://api.github.com/users/${username}/followers?page=${page}&per_page=100`);
+      const followingRes = await axios.get(`https://api.github.com/users/${username}/following?page=${page}&per_page=100`);
 
       const followers = followersRes.data.map(user => user.login);
       const following = followingRes.data.map(user => user.login);
 
       const notFollowingBack = following.filter(user => !followers.includes(user));
-      const notFollowedBack = followers.filter(user => !following.includes(user));
 
       // Generate stats
       const stats = `
       <div class="mt-2 flex flex-col text-sm font-thin  md:flex-row justify-between items-cente p-4 rounded-lg">
         <div class="my-2 md:my-0 md:mx-2 p-2 bg-blue-200 text-blue-700 rounded-lg">
-          <p>Followers: ${followers.length}</p>
+          <p>Followers analyzed: ${followers.length}</p>
         </div>
         <div class="my-2 md:my-0 md:mx-2 p-2 bg-blue-200 text-blue-700 rounded-lg">
-          <p>Following: ${following.length}</p>
+          <p>Following analyzed: ${following.length}</p>
         </div>
         <div class="my-2 md:my-0 md:mx-2 p-2 bg-blue-200 text-blue-700 rounded-lg">
-          <p>Not Following Back: ${notFollowingBack.length}</p>
-        </div>
-        <div class="my-2 md:my-0 md:mx-2 p-2 bg-blue-200 text-blue-700 rounded-lg">
-          <p>Not Followed Back: ${notFollowedBack.length}</p>
+          <p>People Not you Following Back: ${notFollowingBack.length}</p>
         </div>
       </div>
     `;
-    
 
       // Generate HTML for each user not following back
-      const html = notFollowingBack.map(user => `
-        <div class="my-2 p-2 bg-blue-100 rounded-lg">
-          <p class="text-blue-700">${user}</p>
+        const html = notFollowingBack.map(user => `
+        <div class="text-center">
+        <h2 class="text-orange-400 font-mono" > People who do not follow you back </h2>
+        <hr class="bg-black" />
+        <div class="my-2 p-2 bg-blue-100 rounded-lg flex justify-between items-center">
+            <p class="text-blue-700">${user}</p>
+            <a href="https://github.com/${user}" target="_blank" class="text-blue-500 hover:text-blue-800">View Profile</a>
         </div>
-      `).join('');
+        </div>
+        `).join('');
 
       res.status(200).send(stats + html);
     } catch (error) {
-      console.log("An error occurred:", error);
-      res.status(500).json({ error: 'Failed to fetch followers' });
+        const errorMessage = `
+          <div class="my-2 p-2 bg-red-100 rounded-lg">
+            <h2 class="text-red-700 font-bold">An error occurred</h2>
+            <p class="text-red-700">${error.message}</p>
+          </div>
+        `;
+        res.status(200).send(errorMessage);
     }
   } else {
     console.log("Method not allowed");
