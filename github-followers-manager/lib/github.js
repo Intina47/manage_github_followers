02c216a94a0a917137fdaf05d0f2493a581e81ff
mamba <isaiahntina47@gmail.com>
2024-06-04 00:22:32 +0000

    refactor: Fix typo in header field name in GitHubService

--- github-followers-manager/lib/github.js
+++ github-followers-manager/lib/github.js
@@ -2,42 +2,47 @@ const axios = require('axios');
 
 class GitHubService {
     constructor(token) {
+        console.log("GitHubService token: ", token);
         this.token = token;
     }
 
-    async getFollowers(username, page=1) {
+    async getFollowers(username, page) {
         const response = await axios.get(`https://api.github.com/users/${username}/followers?page=${page}&per_page=100`,{
             headers: {
                 'Authorization': `token ${this.token}`,
-                'Accept': 'application/vdn.github.v3+json'
+                'Accept': 'application/vnd.github.v3+json'
             }
         });
         return response.data.map(user => user.login);
     }
 
-    async getFollowing(username, page=1) {
+    async getFollowing(username, page) {
         const response = await axios.get(`https://api.github.com/users/${username}/followers?page=${page}&per_page=100`, {
             header: {
                 'Authorization': `token ${this.token}`,
-                'Accept': 'Application/vdn.github.v3+json'
+                'Accept': 'application/vnd.github.v3+json'
             }
         });
         return response.data.map(user => user.login);
     }
 
-    async getMutualFollowers(username, following) {
-        const mutualFollowers = new set();
+async getMutualFollowers(username, following) {
+    const mutualFollowers = new Set();
 
-        for (const user of following) {
-            const followers = await this.getFollowers(user);
-            followers.array.forEach(follower => {
-                if (followers.includes(follower)) {
-                    mutualFollowers.add(follower);
-                }
-            });
+    for (const user of following) {
+        if (mutualFollowers.size >= 10) {
+            break;
         }
-        return [...mutualFollowers].filter(user => user != username);
+        const followers = await this.getFollowers(user);
+        followers.forEach(follower => {
+            if (followers.includes(follower) && follower != username && mutualFollowers.size < 10) {
+                mutualFollowers.add(follower);
+            }
+        });
     }
+    console.log("Mutual followers:\n ", mutualFollowers);
+    return [...mutualFollowers];
+}
 }
 
 module.exports = GitHubService;
